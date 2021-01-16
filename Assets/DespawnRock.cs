3c1ae9f937861d5162ecaf579fed45f669af7340
AndreiGrumazescu <andrei.grumazescu@mail.utoronto.ca>
2021-01-16 14:14:04 -0500

    rock despawn
    added rock despawn script - dragged onto rock prefab

---
+++ Assets/DespawnRock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DespawnRock : MonoBehaviour
+{
+
+    private GameObject rock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rock = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rock.transform.position.y < -50)
+        {
+            Destroy(rock);
+        }
+
+    }
+}
