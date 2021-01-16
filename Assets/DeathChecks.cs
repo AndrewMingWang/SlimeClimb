d07819d42917b4a8f70103b871a17307637bcf85
AndreiGrumazescu <andrei.grumazescu@mail.utoronto.ca>
2021-01-16 15:30:53 -0500

    final push
    final push

---
+++ Assets/DeathChecks.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathChecks : MonoBehaviour
+{
+
+    public GameObject player;
+    public Text endtext;
+    public Text scoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.x < -15 || player.transform.position.x > 15 || player.transform.position.y < -40)
+        {
+            endtext.text = "You fell off the mountain :c \n" +
+                scoreText.text;
+            Time.timeScale = 0;
+        }
+    }
+}
